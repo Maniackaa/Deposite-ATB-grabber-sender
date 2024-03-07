94dcebab4da99783e0f7e7aadd4454c19ccd89dc
Maniackaa <maniac_kaa@mail.ru>
2024-03-07 21:47:53 +0700

    cahnge log

--- atb_screen_sender.py
+++ atb_screen_sender.py
@@ -39,7 +39,8 @@ def main():
         try:
             global_start = time.perf_counter()
             files = list(path.glob('*.jpg'))
-            logger.info(f'Обнаружены файлы для распознавания:\n{files}')
+            if files:
+                logger.info(f'Обнаружены файлы для распознавания:\n{files}')
             for file in files:
                 try:
                     start = time.perf_counter()
@@ -50,19 +51,18 @@ def main():
                         pass
 
                     elif size > 0:
-                        logger.debug(f'Отправляем {file.name, size, bool(size>0)}')
+                        logger.info(f'Отправляем {file.name, size, bool(size>0)}')
                         with open(file, "rb") as binary:
                             screen = {'image': binary}
                             response = requests.post(OCR_ENDPOINT, data={'name': file.name, 'worker': WORKER, 'oem': '1', 'psm': '7', 'lang': 'eng'}, files=screen, timeout=10)
                             pay = response.reason
-                            logger.debug(f'{response.status_code}')
-                            logger.debug(f'pay: {pay}')
-                            logger.debug(f'Время распознавания {time.perf_counter() - start}')
+                            logger.debug(f'{response.status_code} ({time.perf_counter() - start})')
+                            logger.info(f'Распознан pay: {pay} за {time.perf_counter() - start}')
                         if response.status_code in [200, 201]:
                             # Отправляем платеж
                             # print(file.name)
                             serial = phone_serial(file.name)
-                            logger.info(f'Отправляем скрин с телефона {serial}')
+                            # logger.info(f'Отправляем скрин с телефона {serial}')
                             # print(serial)
                             response = requests.post(ATB_ENDPOINT, data={'pay': pay,
                                                                          'worker': WORKER,
@@ -71,7 +71,7 @@ def main():
                                                                          }, timeout=10)
                             if response.status_code in [200, 201]:
                                 file.unlink()
-                                logger.debug(f'Скрин удален')
+                                logger.debug(f'Платеж отправлен, Скрин удален')
                         elif response.status_code in [400]:
                             file.unlink()
                             logger.debug(f'Скрин не распознан и удален')
@@ -84,9 +84,8 @@ def main():
                     err_log.error(err, exc_info=True)
                     time.sleep(0.1)
 
-            logger.info(f'Общее время: {time.perf_counter() - global_start}')
-            logger.debug('----')
-            time.sleep(0.5)
+            logger.info(f'Все файлы обработаны. Общее время: {time.perf_counter() - global_start}\n')
+            time.sleep(1)
 
         except Exception as err:
             time.sleep(1)
